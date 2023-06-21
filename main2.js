6657988f07c8f06a97f9d3da97a3996afb135a9e
Jeremy <limpinjeremy@gmail.com>
2023-06-21 20:45:18 +0800

    first commit

---
+++ main2.js
@@ -0,0 +1,38 @@
+const ctx2 = document.getElementById('bilog').getContext('2d');
+const bilog =new Chart (ctx2,{
+    type: 'pie',
+    data: {
+      labels: ['Red', 'Blue', 'Yellow', 'Green', 'Purple', 'Orange'],
+      datasets: [{
+        
+        label: '# of Votes',
+        data: [12, 19, 7, 15, 10, 17],
+        backgroundColor: [
+            'rgb(255, 99, 132)',
+            'rgb(75, 192, 192 )',
+            'rgb(255, 205, 86 )',
+            'rgb(0, 128, 0 )',
+            'rgb(153, 102, 255 )',
+            'rgb(239, 108, 0 )',
+            'rgb(201, 203, 207 )'
+          ],
+          borderColor: [
+            'rgb(255, 99, 132)',
+            'rgb(75, 192, 192)',
+            'rgb(255, 205, 86)',
+            'rgb(0, 128, 0)',
+            'rgb(153, 102, 255)',
+            'rgb(255, 213, 79)',
+            'rgb(201, 203, 207)'
+          ],
+          borderWidth: 1
+        }]
+      },
+    options: {
+      scales: {
+        y: {
+          beginAtZero: true
+        }
+      }
+    }
+  });
